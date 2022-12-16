5055f09b920db30095800314d91b7a3938153400
hide00310 <111543047+hide00310@users.noreply.github.com>
2022-12-16 11:25:33 +0900

    no message

---
+++ AddComponentScriptPathEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AddComponentScriptPathEditor : EditorWindow
+{
+    [MenuItem("My Window/Add Component Script")]
+    public static void Execute()
+    {
+        // Cubeを検索
+        var scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+        GameObject Cube = null;
+        foreach (var obj in scene.GetRootGameObjects())
+        {
+            if (obj.name == "Cube") Cube = obj;
+        }
+        // CubeにAddComponentScriptPathScriptコンポーネントを追加
+        AddComponentScript(Cube, "Assets/UdonSharp_AddComponentScriptPath/AddComponentScriptPathScript.cs");
+    }
+    static MonoBehaviour AddComponentScript(GameObject obj, string path)
+    {
+        var script = AssetDatabase.LoadAssetAtPath<MonoScript>(path);
+        Debug.Assert(script != null, $"{obj.name} {path}");
+        MonoBehaviour component = (MonoBehaviour)Undo.AddComponent(obj, script.GetClass());
+        return component;
+    }
+}
